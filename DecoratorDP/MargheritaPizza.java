1726f22a3fbdc14414a88268008e3a80b2cbc213
Nitesh7343 <niteshsingh7343@gmail.com>
2026-08-05 17:54:07 +0530

    created Decorator class and concrete component to start with

---
+++ DecoratorDP/MargheritaPizza.java
@@ -0,0 +1,13 @@
+package DecoratorDP;
+
+public class MargheritaPizza implements Pizza{
+    
+    public String getDesc() {
+        return "Margherita Pizza ";
+    }
+
+    public double price() {
+        return 200.0;
+    }
+
+}
---
+++ DecoratorDP/PizzaDecorator.java
@@ -0,0 +1,8 @@
+package DecoratorDP;
+
+abstract class PizzaDecorator implements Pizza {
+    protected Pizza pizza;
+    PizzaDecorator (Pizza pizza) {
+        this.pizza = pizza;
+    }
+}
